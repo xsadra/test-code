1867483510f96650792095a34bc2cd1a4caa4d17
Sadra <ti9mail@gmail.com>
2018-07-25 11:17:28 +0200

    Finished Task2

--- src/main/java/de/sadrab/testcode/Starter/StarterConfig.java
+++ src/main/java/de/sadrab/testcode/Starter/StarterConfig.java
@@ -1,16 +1,51 @@
 package de.sadrab.testcode.Starter;
 
 import de.sadrab.testcode.Parser.LogParser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class StarterConfig {
+    private RestTemplate rest;
+
+    @Value("${baseUrl.v1}")
+    private String v1Url;
+
+    @Value("${baseUrl.v2}")
+    private String v2Url;
+
     @Bean
     public ApplicationRunner start(LogParser logParser){
         return args -> {
             logParser.parse().forEach(System.out::println);
+
+            List<String> parsedData = logParser.parse();
+
+            List<String> requests = parsedData.stream().filter(item -> item.contains("GET")).collect(Collectors.toList());
+
+            requests.forEach(request -> {
+                String requestData = request.split(" /")[1];
+
+                String response1 = sendRequest(v1Url, requestData);
+                String response2 = sendRequest(v2Url, requestData);
+
+                System.out.println(response1);
+                System.out.println(response2);
+            });
+
+
         };
+    }
+
+    private String sendRequest(String url, String path) {
+        String requestUrl1 = url + path;
+
+        return rest.getForObject(requestUrl1, String.class);
     }
 }
---
+++ src/main/java/de/sadrab/testcode/configuration/RestConfiguration.java
@@ -0,0 +1,13 @@
+package de.sadrab.testcode.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfiguration {
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
