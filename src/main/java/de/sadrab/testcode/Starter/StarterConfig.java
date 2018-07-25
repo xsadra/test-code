705d321fb3ff65decec572e277556ddbf8375450
Sadra <ti9mail@gmail.com>
2018-07-25 12:57:30 +0200

    Finished Task4/5

--- src/main/java/de/sadrab/testcode/Starter/StarterConfig.java
+++ src/main/java/de/sadrab/testcode/Starter/StarterConfig.java
@@ -1,12 +1,18 @@
 package de.sadrab.testcode.Starter;
 
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.sadrab.testcode.Parser.LogParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -39,14 +45,49 @@ public class StarterConfig {
                 String response1 = sendRequest(v1Url, requestData);
                 String response2 = sendRequest(v2Url, requestData);
 
-                //System.out.println(response1);
-                //System.out.println(response2);
+//                System.out.println(response1);
+//                System.out.println(response2);
 
                 String message = (response1.equalsIgnoreCase(response2)) ?
                         (" == With [" + requestData + "] have identical output. ==") :
                         " ## With [" + requestData + "] have different output. ##";
 
                 System.out.println(message);
+
+
+
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+                    JsonNode jsonNode1 = objectMapper.readTree(response1);
+                    JsonNode jsonNode2 = objectMapper.readTree(response2);
+
+                    if (jsonNode1 != null && jsonNode2 != null) {
+
+                        if (jsonNode1.equals(jsonNode2)) {
+                            System.out.println("Equle");
+                        }
+
+                        Iterator<String> stringIterator = jsonNode1.fieldNames();
+
+                        stringIterator.forEachRemaining(fildName -> {
+                            if (jsonNode1.get(fildName).equals(jsonNode2.get(fildName))) {
+                                System.out.println("= " + fildName + " in first Equle to second");
+                            }
+                            else {
+                                System.out.println("# " +  fildName + " in first NOT Equle to second");
+                            }
+
+                        });
+
+                         //
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             });
 
 
@@ -56,6 +97,10 @@ public class StarterConfig {
     private String sendRequest(String url, String path) {
         String requestUrl1 = url + path;
 
-        return rest.getForObject(requestUrl1, String.class);
+        try {
+            return rest.getForObject(requestUrl1, String.class);
+        }catch (HttpClientErrorException e){
+            return "";
+        }
     }
 }
