98538c3a270e3309dadf92352201a0f1969b61c3
Sadra <ti9mail@gmail.com>
2018-07-25 10:08:28 +0200

    add Maven Structure

---
+++ src/main/java/de/sadrab/testcode/TestCodeApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.testcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCodeApplication.class, args);
+	}
+}
